3c6508dbc17a20248e68c4194e91146e2080f002
DenisKurlykov <dkurlykov@gmail.com>
2022-09-27 00:37:25 +0400

    Выполнение ДЗ 5

--- CourseOne/main.swift
+++ CourseOne/main.swift
@@ -7,5 +7,90 @@
 
 import Foundation
 
-print("Hello, World!")
+// 3 - Создайте пустой словарь responseMessages, для ключа тип числовой, для значения строка.
+var responseMessages: [Int: String] = [:]
+
+// 4 - Добавьте в пустой cловарь ключи 500, 404, 403, 200, а значения "OK", "Accsess forbiden", "File not found", "Internal server error"
+responseMessages = [500 : "OK"]
+responseMessages.updateValue("Accsess forbiden", forKey: 404)
+responseMessages[403] = "File not found"
+responseMessages[200] = "Internal server error"
+
+// 5 - Создайте массив httpResponseCodes c числами 200, 403, 301
+let httpResponseCodes = [200, 403, 301]
+
+// 6 - Переберите массив httpResponseCode в цикле чтоб при каждой итерации вы проверяли код массива в словаре responseMessage и если код такой есть то выводить в консоль ключ и значение, если нет писать неизвестный код и выводить код.
+for code in httpResponseCodes {
+    if (responseMessages[code] != nil) {
+        print("Код \(code):  \(responseMessages[code] ?? "")")
+    } else {
+       print("Неизвестный код: \(code)")
+    }
+}
+
+// 7 - Отсортируйте все ключи по порядку 200, 403 и тд.
+let sorted = responseMessages.keys.sorted()
+print(sorted)
+
+// 8 - Распечатайте в цикле все ключи и значения.
+for (key, value) in responseMessages {
+    print("Ключ: \(key); Значение: \(value)")
+}
+print("")
+for key in responseMessages.keys {
+    print("Ключ: \(key); Значение: \(responseMessages[key] ?? "Описание кода отсутсвует")")
+}
+
+// 9 - Создайте словарь с ключами primes, triangular, hexagonal, у каждого ключа значение это массив [2, 13, 15, 7, 11, 3, 5], у второго значения массив [21, 6, 3, 10, 15, 1, 28], у третьего [1, 91, 15, 28, 45, 66, 6]
+var shapes = [
+    "primes" : [2, 13, 15, 7, 11, 3, 5],
+    "triangular" : [21, 6, 3, 10, 15, 1, 28],
+    "hexagonal" : [1, 91, 15, 28, 45, 66, 6]
+]
+
+// 10 - Отсортируйте содержимое каждого массива по возрастанию.
+for value in shapes.keys {
+    shapes[value]?.sort()
+}
+print(shapes)
+
+// 11 - Создать 2 словаря в 1 ключ и значение «a»: «b», у второго «c»: «d» нужно объединить эти 2 словаря не используя +=.
+var dictOne = ["a" : "b"]
+let dictTwo = ["c" : "d"]
+
+dictOne.merge(zip(dictTwo.keys, dictTwo.values)) { (curent, _) in
+    curent
+}
+
+// Вариант 1
+dictOne.merge(dictTwo) { curent, _ in
+    curent
+}
+print(dictOne)
+
+// Вариант 2
+let dictThree = dictTwo.merging(dictOne) { curent, _ in
+    curent
+}
+print(dictThree)
+
+// 12 - Создать переменную а = 1 и вторую b = 2, поменяйте местами значения у 2х переменных 2 мя способами.
+var a = 1
+var b = 2
+// Вариант 1
+var c: Int
+c = a
+a = b
+b = c
+print(a)
+print(b)
+
+// Вариант 2
+swap(&a, &b)
+print(a)
+print(b)
+
+// 13 - Опишите что такое словарь, почему его иногда называют хеш мап, и это class или struct.
+
+// 14 - Напишите какие протоколы комформит словарь и для чего они (только кратко).
 
